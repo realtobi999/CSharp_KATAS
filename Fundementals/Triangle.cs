e2885fb893612b9b125ea77f2cdfb2e21a703a97
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-17 18:44:03 +0200

    [fix] IsTriangle kata

--- Fundementals/Triangle.cs
+++ Fundementals/Triangle.cs
@@ -8,12 +8,14 @@ public class Triangle
             The function should return true if a triangle can be built
             with the sides of given length and false in any other case.
         Rešení:
-            We just check if the sum of a and b is less than c. In that
-            case we return false else true
+            We just check if the two sides are always bigger than the third
+            side, if atleast one condition is true we return false, because
+            one side is bigger than the sum of the other two. Otherwise we 
+            return true, because the triangle is valid.
     */
     public static bool IsTriangle(int a, int b, int c)
     {
-        if (a + b < c) {
+         if (a + b <= c || a + c <= b || b + c <= a) {
             return false;
         }
 
