2838ce22159d6ab96acc490cd1b2921f47039538
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-17 18:38:19 +0200

    [kata] IsTriangle

---
+++ Fundementals/Triangle.cs
@@ -0,0 +1,22 @@
+﻿namespace Fundementals;
+
+public class Triangle
+{
+    /*
+        Zadání:
+            Implement a function that accepts 3 integer values a, b, c. 
+            The function should return true if a triangle can be built
+            with the sides of given length and false in any other case.
+        Rešení:
+            We just check if the sum of a and b is less than c. In that
+            case we return false else true
+    */
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        if (a + b < c) {
+            return false;
+        }
+
+        return true;
+    }
+}
