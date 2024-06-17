98649588c0d1d106e99305e3195b169ff8cc1a40
realtobi999 <tobiasfilgas@gmail.com>
2024-06-17 21:11:20 +0200

    [kata] MostFrequentWordInTextKata

---
+++ C#/Algorithms/MostFrequentWordInTextKata.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algorithms;
+
+public class MostFrequentWordInTextKata
+{
+    public static List<string> Top3(string s)
+    {
+        var matches = Regex.Matches(s, @"([A-Za-z][A-Za-z']*)");
+        var queryResult = matches.Select(x => x.Value.ToLower())
+                                 .GroupBy(x => x)
+                                 .Select(x => new { Key = x.Key, Count = x.Count() })
+                                 .OrderByDescending(x => x.Count)
+                                 .Take(3)
+                                 .Select(x => x.Key)
+                                 .ToList();
+
+        // Your code here
+        return queryResult;
+    }
+}
